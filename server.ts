a95535c39a790f6d9464ff43cfb2b39a1a56d15d
hemadhami001 <hemadhami365@gmail.com>
2025-06-10 08:13:50 +0545

    initial

--- server.ts
+++ server.ts
@@ -6,8 +6,9 @@ import "./src/database/connection"
 // database connection
 // This code imports the Express application from app.ts and the database connection from connection.ts.
 
+
 function startServer() {
-    const port = process.env.PORT 
+    const port = process.env.PORT    // Get the port from environment variables
     app.listen(port,function(){
         console.log('Server has started at port ${port}')
     })
